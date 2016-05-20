5ec38ccc1c2f58be7dd4bf329640f13d965d8136
K <luca@pretto.eu>
2016-05-20 15:06:31 +0200

    Create mysql-separate-dump-settings.sh

---
+++ mysql-separate-dump-settings.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Set here your preferences for the script in mysql-separate-dump.sh
+
+# Backup Directory (without trailing slash)
+MSD_DEST_FOLDER="/srv/db-backup"
+
+# MySQL credentials
+MSD_SQL_USER="root"
+MSD_SQL_PASSWORD="masterkey"
