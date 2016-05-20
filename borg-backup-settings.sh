e74936031cf5c1a608d11d5d92631fac2177ae0d
K <luca@pretto.eu>
2016-05-20 10:45:50 +0200

    Create borg-backup-settings.sh

---
+++ borg-backup-settings.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Set here your preferences for the script in /usr/local/bin/borg-backup.sh
+
+# Mandatory settings
+BORG_PASSPHRASE="YourPassword"
+BORG_REPOSITORY="ssh://root@borg.YourDomain:2222/opt/borg/YourPath"
+BORG_SOURCE="/"
+
+# Data retention
+BORG_KEEP_DAILY="14"
+BORG_KEEP_WEEKLY="6"
+BORG_KEEP_MONTHLY="1"
